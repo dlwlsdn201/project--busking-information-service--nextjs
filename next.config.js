a3f5c16854320deba03e8fa903b83dde57306de3
JinWoo_Lee <dlwlsdn201@naver.com>
2025-03-08 22:52:50 +0900

    :tada: first commit !!

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   env: {},
 
   webpack: (config) => {
     return config;
+  },
+
+  redirects: async () => {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: true,
+      },
+    ];
   },
 };
 
--- shared/config/Navigation.ts
+++ shared/config/Navigation.ts
@@ -5,11 +5,19 @@ interface Navigation {
 
 export const NavigationList: Navigation[] = [
   {
-    path: "/",
-    label: "홈",
+    path: '/dashboard',
+    label: '대시보드',
   },
   {
-    path: "/about-us",
-    label: "소개",
+    path: '/busking-location',
+    label: '버스킹 장소',
+  },
+  {
+    path: '/busking-event',
+    label: '버스킹 공연 홍보',
+  },
+  {
+    path: '/setting',
+    label: '환경설정',
   },
 ];
