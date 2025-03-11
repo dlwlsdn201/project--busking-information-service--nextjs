2f3c490a145543d3b8ac01c203636ec844611067
JinWoo_Lee <dlwlsdn201@naver.com>
2025-03-11 23:08:21 +0900

    :lipstick: Navigation Sidebar UI 신규 구현

--- src/shared/config/Navigation.ts
+++ src/shared/config/Navigation.ts
@@ -1,23 +1,38 @@
+import {
+  Icon,
+  IconCalendar,
+  IconChartBarPopular,
+  IconMap2,
+  IconProps,
+  IconSettings,
+} from '@tabler/icons-react';
+import { ForwardRefExoticComponent, RefAttributes } from 'react';
+
 interface Navigation {
   path: string;
   label: string;
+  icon: ForwardRefExoticComponent<IconProps & RefAttributes<Icon>>;
 }
 
-export const NavigationList: Navigation[] = [
+export const ROUTE_LIST: Navigation[] = [
   {
     path: '/dashboard',
     label: '대시보드',
+    icon: IconChartBarPopular,
   },
   {
     path: '/busking-location',
     label: '버스킹 장소',
+    icon: IconMap2,
   },
   {
     path: '/busking-event',
     label: '버스킹 공연 홍보',
+    icon: IconCalendar,
   },
   {
     path: '/setting',
     label: '환경설정',
+    icon: IconSettings,
   },
 ];
---
+++ src/shared/ui/navigation/Navigation.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { NavLink } from '@mantine/core';
+import { usePathname } from 'next/navigation';
+import { ROUTE_LIST } from '@shared/config/Navigation';
+import { NavigationContainer } from './Navigation.styled';
+
+export const Navigation = ({}): React.ReactElement => {
+  const currentPath = usePathname();
+
+  const navigationItems = ROUTE_LIST.map((item) => {
+    const IconComponent = item.icon;
+    return (
+      <NavLink
+        active={item.path === currentPath}
+        key={item.path}
+        href={item.path}
+        label={item.label}
+        leftSection={<IconComponent />}
+        variant="light"
+        color="#5542d0"
+        autoContrast
+      />
+    );
+  });
+
+  return <NavigationContainer>{navigationItems}</NavigationContainer>;
+};
